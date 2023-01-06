a807fdbc68e0ba899d9a66d6668881cd4bcb07e0
marcr <marcrogge@arcor.de>
2023-01-06 19:58:54 +0100

    V1.001

--- app/src/main/java/com/appzzzz/vokabeltrainer/data/Vocabulary.kt
+++ app/src/main/java/com/appzzzz/vokabeltrainer/data/Vocabulary.kt
@@ -2,4 +2,11 @@ package com.appzzzz.vokabeltrainer.data
 
 data class Vocabulary(
     val germanVocabulary: String = "",
-    val englishVocabulary: String = "")
+    val englishVocabulary: String = "",
+    val firstLetterCore: Int = 0,
+    val lastLetterCore: Int = 0,
+    val englishVocabularyCore: String = "",
+    val vocabularyType: String = "",
+    val irregularVerbInfinitive: String = "",
+    val irregularVerbSimplePast: String = "",
+    val irregularVerbPastParticiple: String = "")
